b2eb0ef2446d0198f544eb422f31c11f8d1dea11
SlippyJohny <shpak.d.o@ukr.net>
2017-01-24 14:46:25 +0200

    changes in mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,17 +4,6 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Mail.defaults do
-  delivery_method :smtp, {
-      :address => ENV['POSTMARK_SMTP_SERVER'],
-      :port => '25', # or 2525
-      :domain => 'mailersystem.heroku.com',
-      :user_name => ENV['POSTMARK_API_TOKEN'],
-      :password => ENV['POSTMARK_API_TOKEN'],
-      :authentication => :cram_md5, # or :plain for plain-text authentication
-      :enable_starttls_auto => true, # or false for unencrypted connection
-  }
-end
 
 ActionMailer::Base.smtp_settings = {
     :port           => '25', # or 2525
