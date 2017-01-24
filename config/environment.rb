21b26bafd6f50f09189a16f5e1790df80940cb96
SlippyJohny <shpak.d.o@ukr.net>
2017-01-24 14:16:56 +0200

    environment.rb changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :address => 'mailersystem.herokuapp.com',
+    :port => 587,
+    :user_name => "shpak.d.o@gmail.com",
+    :password => "dmitro24",
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+    :openssl_verify_mode => 'none'
+}
