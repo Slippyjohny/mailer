b51a871665f57fee324e64028ad61f86721e0a29
SlippyJohny <shpak.d.o@ukr.net>
2017-01-24 14:39:00 +0200

    sm changes in env

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,27 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+Mail.defaults do
+  delivery_method :smtp, {
+      :address => ENV['POSTMARK_SMTP_SERVER'],
+      :port => '25', # or 2525
+      :domain => 'mailersystem.heroku.com',
+      :user_name => ENV['POSTMARK_API_TOKEN'],
+      :password => ENV['POSTMARK_API_TOKEN'],
+      :authentication => :cram_md5, # or :plain for plain-text authentication
+      :enable_starttls_auto => true, # or false for unencrypted connection
+  }
+end
+
 ActionMailer::Base.smtp_settings = {
-    :address => '0.0.0.0',
-    :port => 587,
-    :user_name => "shpak.d.o@gmail.com",
-    :password => "dmitro24",
-    :authentication => :plain,
-    :enable_starttls_auto => true,
-    :openssl_verify_mode => 'none'
+    :port           => '25', # or 2525
+    :address        => ENV['POSTMARK_SMTP_SERVER'],
+    :user_name      => ENV['POSTMARK_API_TOKEN'],
+    :password       => ENV['POSTMARK_API_TOKEN'],
+    :domain         => 'mailersystem.heroku.com',
+    :authentication => :cram_md5, # or :plain for plain-text authentication
+    :enable_starttls_auto => true, # or false for unencrypted connection
 }
+ActionMailer::Base.delivery_method = :smtp
+
+
